de8fce939874854df9998e11c7b8b65f7be7621a
yn <jenny783113@gmail.com>
2025-08-25 16:00:10 +0800

    前台 完成結帳訂單(填寫資料)畫面

---
+++ front/checkout.php
@@ -0,0 +1,56 @@
+<?php
+$user=$User->find(['acc'=>$_SESSION['login']]);
+
+?>
+
+<h2 class="ct">填寫資料</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">登入帳號</td>
+        <td class="pp"><?=$user['acc'];?></td>
+    </tr>
+    <tr>
+        <td class="tt ct">姓名</td>
+        <td class="pp"><input type="text" name="name" id="name" value="<?=$user['name'];?>"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電子信箱</td>
+        <td class="pp"><input type="text" name="email" id="email" value="<?=$user['email'];?>"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">聯絡地址</td>
+        <td class="pp"><input type="text" name="addr" id="addr" value="<?=$user['addr'];?>"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">聯絡電話</td>
+        <td class="pp"><input type="text" name="tel" id="tel" value="<?=$user['tel'];?>"></td>
+    </tr>
+</table>
+
+<!-- "buycart.php"copy過來改的 -->
+<table class="all">
+    <tr class="tt ct">
+        <td>商品名稱</td>
+        <td>編號</td>
+        <td>數量</td>
+        <td>單價</td>
+        <td>小計</td>
+    </tr>
+    <?php
+    $sum = 0; //總價
+    foreach($_SESSION['cart'] as $id => $qt):
+        $item=$Item->find($id);
+    ?>
+        <tr class="pp ct">
+            <td><?=$item['name'];?></td>
+            <td><?=$item['no'];?></td>
+            <td><?=$qt;?></td>
+            <td><?=$item['price'];?></td>
+            <td><?=$item['price']*$qt;?></td>
+        </tr>
+    <?php
+        $sum += $item['price']*$qt;
+    endforeach;
+    ?>
+</table>
+<div class="all tt ct">總價：<?=$sum;?></div>
