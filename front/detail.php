b9dcdf0b72111c306a8bfa56bc0a4e744def3f8c
yn <jenny783113@gmail.com>
2025-08-25 11:39:37 +0800

    前台 完成商品陳列及商品詳細資料 畫面顯示

---
+++ front/detail.php
@@ -0,0 +1,45 @@
+<?php
+$item = $Item->find($_GET['id']);
+?>
+
+<h2 class="ct"><?=$item['name'];?></h2>
+<div style="display: flex; width:80%; margin:5px auto;">
+    <div class="pp ct" style="padding: 10px; width:65%;">
+        <a href="?do=detail&id=<?= $item['id']; ?>">
+            <img src="./image/<?= $item['img']; ?>" style="width:100%; height:100%;">
+        </a>
+    </div>
+    <div style="width:35%;">
+        <!-- table>(tr.tt.ct>td)+(tr.pp*3>td) -->
+        <table style="height: 100%;">
+            <tr>
+                <td class="pp">分類：</td>
+            </tr>
+            <tr class="pp">
+                <td>編號：<?= $item['no'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    價錢：<?= $item['price'];?>
+                </td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    詳細說明：<?=$item['intro'];?>
+                </td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    庫存：<?=$item['stock'];?>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="tt ct"  style="width:80%; margin:auto;">
+    購買數量：
+    <input type="number" name="qt" value="1">
+    <a href="?do=buycart&id=<?=$item['id'];?>">
+        <img src="./icon/0402.jpg" alt="">
+    </a>
+</div>
--- front/main.php
+++ front/main.php
@@ -1,8 +1,7 @@
 <?php
 
-//"麵包屑"區 start
 $type = $_GET['type']??0;
-$nav = '全部商品';
+$nav = '全部商品'; //"麵包屑"區
 if($type != 0){
     $row = $Type->find($type);
     if($row['big_id']==0){ //是大分類
@@ -17,7 +16,6 @@ if($type != 0){
     $items = $Item->all(['sh'=>1]);
 
 }
-//"麵包屑"區 end
 
 ?>
 
@@ -25,11 +23,40 @@ if($type != 0){
 <h2><?=$nav;?></h2>
 
 <?php
-foreach($items as $item){
-    echo $item['name'];
-    echo "<br>";
-}
+foreach($items as $item):
+?>
+<div style="display: flex; width:65%; height:150px; margin:5px auto;">
+    <div class="pp ct" style="padding: 20px;">
+        <a href="?do=detail&id=<?=$item['id'];?>">
+            <img src="./image/<?=$item['img'];?>" style="width:150px; height:100px;">
+        </a>
+    </div>
+    <div>
+        <!-- table>(tr.tt.ct>td)+(tr.pp*3>td) -->
+        <table style="height: 100%;">
+            <tr>
+                <td class="tt ct"><?=$item['name'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    價錢：<?=$item['price'];?>
+                    <a href="?do=buycart&id=<?=$item['id'];?>&qt=1"> <!-- qt=1 預設要選購的商品數量為1-->
+                        <img src="./icon/0402.jpg" style="float:right;">
+                    </a>
+                </td>
+            </tr>
+            <tr class="pp">
+                <td>規格：<?=$item['spec'];?></td>
+            </tr>
+            <tr class="pp">
+                <td>
+                    簡介：<?=mb_substr($item['intro'], 0, 20);?>...
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
 
-
-
+<?php
+endforeach;
 ?>
