68064f1004be6d1af24816fb331dadc3ff1b082a
yn <jenny783113@gmail.com>
2025-08-25 14:19:19 +0800

    前台 建立購物車session

--- front/buycart.php
+++ front/buycart.php
@@ -1,4 +1,8 @@
 <?php
+if(isset($_GET['id'])){ //有沒有點東西 進到購物車
+    $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
+}
+
 if(!isset($_SESSION['login'])){
     to("?do=login");
 }
@@ -6,3 +10,11 @@ if(!isset($_SESSION['login'])){
 
 
 <h2 class="ct"><?=$_SESSION['login'];?>的購物車</h2>
+
+<?php
+if(isset($_SESSION['cart']) && count($_SESSION['cart'])>0){
+    dd($_SESSION['cart']);
+}else{
+    echo "購物車是空的";
+}
+?>
