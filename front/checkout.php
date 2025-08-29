62282980b7380da646f1d74a78371cabeb29e21b
yn <jenny783113@gmail.com>
2025-08-29 10:48:46 +0800

    (使用e.preventDefault()來完成表單送出前提示)=>有問題&(使用FormData物件來取得表單的內容並以ajax送出)

--- front/checkout.php
+++ front/checkout.php
@@ -57,14 +57,49 @@ $user=$User->find(['acc'=>$_SESSION['login']]);
     <div class="all tt ct">總價：<?=$sum;?></div>
     <input type="hidden" name="total" value="<?=$sum;?>">
     <div class="ct">
-        <input type="button" value="確定送出" onclick="submitForm()">
+        <!-- <input type="button" value="確定送出" onclick="submitForm()"> -->
+        <input type="submit" value="確定送出">
         <input type="button" value="返回修改訂單" onclick="location.href='?do=buycart'">
     </div>
 
 </form>
 <script>
-    function submitForm(){
+    // "訂購成功提示訊息" way 3
+    $("#orderForm").on("submit", function(e){
+        e.preventDefault();
+        let form = new FormData(document.getElementById("orderForm"));
+        let data = Object.fromEntries(form.entries());
+        // console.log(data);
+        $.post("./api/save_order.php", data,()=>{
+            alert("訂購成功\n感謝您的選購");
+            location.href='?';
+        });
+    });
+
+    // "訂購成功提示訊息" way 2 + chatGPT解決無限循環觸發alert問題
+    // $("#orderForm").on("submit", function(e){
+    //     e.preventDefault();
+    //     alert("訂購成功\n感謝您的選購");
+        
+    //     // 老師寫(有問題 他沒實作所以沒發現)
+    //     // $("#orderForm").submit();
+
+    //     // chatGPT解法1
+    //     /* // 解除 submit handler，避免遞迴觸發
+    //     $("#orderForm").off("submit");
+    //     // 手動送出表單
+    //     $("#orderForm")[0].submit(); */
+
+    //     // chatGPT解法2
+    //     setTimeout(function() {
+    //         $("#orderForm")[0].submit();  // 延遲執行，避免干擾 alert 行為
+    //     }, 100);
+    // });
+
+    
+    // "訂購成功提示訊息" way 1
+    /* function submitForm(){
         alert("訂購成功\n感謝您的選購");
         $("#orderForm").submit();
-    }
+    } */
 </script>
